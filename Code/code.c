bb602c3b18b6bada1ed57d4c578b8091cda2b800
gmalavazi <86890976+gmalavazi@users.noreply.github.com>
2021-07-20 19:24:09 -0300

    Update code.c

--- Code/code.c
+++ Code/code.c
@@ -24,3 +24,31 @@ void main()
   
   // Get I2C device, Address of the BME280 I2C bus is 0x76(136)
 	ioctl(arch, I2C_SLAVE, 0x76);
+  
+  //Read 24 bytes of data from register (0x88)
+  char rg1[1] = {0x88};
+  write(arch, rg1, 1);
+  char bin1[24] = {0};
+  if(read(arch,bin1,24) != 24)
+  {	
+	  printf("Error: Input/Output error \n");
+	  exit(1);
+  }
+
+  // Convert data, temp coefficents
+  int digital_Temp1 = (bin1[0] + bin1[1]*256);
+  int dgitial_Temp2= (bin1[2] + bin[3] * 256);
+  if(digital_Temp2 > 32767)
+  {
+	  digital_Temp2 -= 65536;
+  }
+  int digital_Temp3 = (bin1[4] + bin1[5] *256);
+  if(digital_Temp3 > 32767)
+  {
+	  digital_Temp3 -= 65536;
+  }
+	
+
+  
+	
+	
